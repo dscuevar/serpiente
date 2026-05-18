b2bf48d79981d710a86a3fa6e6bfef6fdaff72b7
dscuevar <sbascueva@gmail.com>
2026-05-17 22:52:36 -0500

    parte 1 lista

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,46 @@
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+
+const TAMANIO_CELDA = 25;
 
 
-    
+function dibujarTodo() {
+  limpiarCanvas();
+  dibujarTablero();
+}
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
+function limpiarCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
+function dibujarTablero() {
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "black";
+
+    // Líneas verticales
+    for(let x = 0; x <= canvas.width; x += TAMANIO_CELDA){
+
+        ctx.beginPath();
+
+        ctx.moveTo(x, 0);
+
+        ctx.lineTo(x, canvas.height);
+
+        ctx.stroke();
     }
 
-    function dibujarTodo() {
-      limpiarCanvas();
+    // Líneas horizontales
+    for(let y = 0; y <= canvas.height; y += TAMANIO_CELDA){
+
+        ctx.beginPath();
+
+        ctx.moveTo(0, y);
+
+        ctx.lineTo(canvas.width, y);
+
+        ctx.stroke();
     }
+}
 
-
-
+dibujarTodo();
